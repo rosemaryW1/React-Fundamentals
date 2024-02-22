a0c87f8bf283b544709858e2cca1a976d2461aff
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2024-02-22 16:16:26 +0300

    use of map method to access properties of an object and render them using a component

--- src/index.js
+++ src/index.js
@@ -2,21 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css'
-const Books = [
+const books = [
   {
     author:'Laura Nowlin',
     title:'If He Had Been with Me',
-    img:"https://images-na.ssl-images-amazon.com/images/I/61tdwW3Nm8L._AC_UL600_SR600,400_.jpg"
+    img:"https://images-na.ssl-images-amazon.com/images/I/61tdwW3Nm8L._AC_UL600_SR600,400_.jpg",
+    id:1,
    },
    {
      author:'Jamie Kern Lima ',
      title:'Worthy',
-     img:"https://images-na.ssl-images-amazon.com/images/I/71WwD1Uw8QL._AC_UL600_SR600,400_.jpg"
-    },
+     img:"https://images-na.ssl-images-amazon.com/images/I/71WwD1Uw8QL._AC_UL600_SR600,400_.jpg",
+     id:2,
+    }
 ];
 
 const BookList = () =>{
-  return <section className='booklist'></section>
+  return <section className='booklist'>
+    {books.map((book)=>{
+      const { img, title, author, id} = book
+      return (
+        <Book img={img} title={title} author={author} 
+        key = {id}/>
+      )
+    })}
+  </section>
 }
 const Book = ({img,title,author,}) => {
    
